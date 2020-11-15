f7c38ba9b3f65603ee3d3de9cdf15614fad0f0d7
andikasetyono <74491365+andikasetyono@users.noreply.github.com>
2020-11-15 20:19:18 +0700

    Update kia.py

--- kia.py
+++ kia.py
@@ -12,15 +12,22 @@ from googletrans import Translator
 #
 botStart = time.time()
 
-Kia = LINE()
-#Kia = LINE('Token')
-#Kia = LINE("Email","Paswot")
-Kia.log("Auth Token : " + str(Kia.authToken))
-channelToken = Kia.getChannelResult()
-Kia.log("Channel Token : " + str(channelToken))
-
-readOpen = codecs.open("read.json","r","utf-8")
-settingsOpen = codecs.open("temp.json","r","utf-8")
+#try:
+   # header = "ios_ipad"
+  #  auth = "Z6vMBEnkp04n"
+  #  result = json.loads(requests.get("https://api.boteater.us/line_qr_v2?header="+header+"&auth="+auth).text)
+  #  print("Login IP: {}".format(result["result"]["login_ip"]))
+   # print("QR Link: {}".format(result["result"]["qr_link"]))
+  #  result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
+  #  if result["status"] != 200:
+     # raise Exception("Timeout!!!")
+  #  print("Pincode: "+result["result"]["pin_code"])
+   # result = json.loads(requests.get(result["result"]["callback"]+"&auth="+auth).text)
+    # if result["status"] != 200:
+    #  raise Exception("Timeout!!!")
+   # Kia = LINE(result["result"]["token"],appName="IOSIPAD\t9.18.1\tiPhone X\t12.4.1")
+   # print("Login Sukses")
+#except:pass
 
 KiaMID = Kia.profile.mid
 KiaProfile = Kia.getProfile()
